bdbf6fe6f131444cc0b33cd87e9a196acc23c07e
YuKazami <antonbischof@pm.me>
2023-12-07 00:19:28 +0100

    feat: getting close

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let game_data;
 let localization_data;
 let cy;
+let selected_pms = {};
 
 const paradoxIconsPath = "./images/paradox/gfx/interface/icons/"
 
@@ -115,7 +116,7 @@ async function refresh_edges(selected_pms) {
         for (const [i_key, i_val] of Object.entries(inputs)) {
             const cost = game_data["goods"][i_key]["cost"]
 
-            const worth = cost ? i_val*cost+'£' : 0
+            const worth = cost ? i_val * cost + '£' : 0
             cy.add({
                 group: 'edges',
                 data: {source: i_key, target: building_key, name: worth}
@@ -125,7 +126,7 @@ async function refresh_edges(selected_pms) {
         for (const [o_key, o_val] of Object.entries(outputs)) {
             const cost = game_data["goods"][o_key]["cost"]
 
-            const worth = cost ? o_val*cost+'£' : 0
+            const worth = cost ? o_val * cost + '£' : 0
             cy.add({
                 group: 'edges',
                 data: {source: building_key, target: o_key, name: worth}
@@ -134,9 +135,9 @@ async function refresh_edges(selected_pms) {
     }
 }
 
-async function remove_isolated_nodes(){
+async function remove_isolated_nodes() {
     cy.nodes().each(node => {
-        if(node.connectedEdges().length === 0){
+        if (node.connectedEdges().length === 0) {
             cy.remove(node)
         }
     })
@@ -152,20 +153,14 @@ async function graph_controller() {
     //region Temp
     let selected_buildings = ["building_food_industry", "building_textile_mills", "building_tooling_workshops", "building_paper_mills"]
     selected_buildings = Object.keys(game_data["buildings"])
-    for(const building of selected_buildings){
+    for (const building of selected_buildings) {
         const pmgs = game_data["buildings"][building]["production_method_groups"]
         let pms = []
-        for(const pmg of pmgs){
-             pms.push(game_data["production_method_groups"][pmg]["production_methods"][0])
+        for (const pmg of pmgs) {
+            pms.push(game_data["production_method_groups"][pmg]["production_methods"][0])
         }
         selected_pms[building] = pms
     }
-    //selected_pms["building_food_industry"] = ["pm_baking_powder", "pm_cannery", "pm_pot_stills"]
-    //selected_pms["building_textile_mills"] = []
-    //selected_pms["building_tooling_workshops"] = ["pm_steel", "pm_automation_disabled", "pm_merchant_guilds_building_tooling_workshops"]
-    //selected_pms["building_paper_mills"] = ["pm_bleached_paper"]
-    //delete selected_pms["building_tooling_workshops"]
-    //delete selected_pms["building_iron_mine"]
 
     //endregion
 
@@ -199,28 +194,29 @@ async function graph_controller() {
     }).run()
 }
 
-function getLocalized(word){
+function getLocalized(word) {
     let localized = localization_data[word]
     if (!localized) {
         return word
     }
 
-    if(localized.includes('$')){
+    if (localized.includes('$')) {
         localized = localization_data[localized.replaceAll('$', '')]
     }
 
     return localized
 }
 
-async function main(){
+async function main() {
     await fetch_files()
 
     await graph_controller()
 
     //neues Zeug
     const graph_setting = document.getElementById('graph-settings-body')
-    for(const [building, value] of Object.entries(game_data["buildings"])){
+    for (const [building, value] of Object.entries(game_data["buildings"])) {
         const building_element = document.createElement('building-settings')
+        building_element.dataset.buildingKey = building
 
         const buildingNameSlot = createSlot('span', 'buildingName', building_element);
         buildingNameSlot.innerText = getLocalized(building)
@@ -248,7 +244,7 @@ async function main(){
         const buildingPmgsElement = building_element.shadowRoot.getElementById('buildingPmgs')
         const pmgs = value['production_method_groups']
 
-        for(const pmg of pmgs){
+        for (const pmg of pmgs) {
             const pmg_loc_name = getLocalized(pmg)
             const pmgElement = document.createElement('pmg-choicebox')
             const pmgPmsElement = pmgElement.shadowRoot.getElementById('pmgPms')
@@ -258,13 +254,16 @@ async function main(){
 
             const pms = game_data['production_method_groups'][pmg]['production_methods']
             let first = true
-            for(const pm of pms){
+            for (const pm of pms) {
                 const pm_loc_name = getLocalized(pm)
                 const pmButtonElement = document.createElement('input')
                 pmButtonElement.name = pmg
                 pmButtonElement.type = 'radio'
                 pmButtonElement.id = 'radio_button_' + pm
                 pmButtonElement.classList.add('pm-radio-button')
+                //pmButtonElement.dataset.buildingKey = building
+                //pmButtonElement.dataset.pmgKey = pmg
+                pmButtonElement.dataset.pmKey = pm
 
                 const pmLabelElement = document.createElement('label')
                 pmLabelElement.setAttribute('for', pmButtonElement.id)
@@ -275,7 +274,7 @@ async function main(){
 
                 pmLabelElement.append(pmImgElement)
 
-                if(first){
+                if (first) {
                     pmButtonElement.checked = true
                     first = false
                 }
@@ -289,26 +288,42 @@ async function main(){
 
     graph_setting.addEventListener('click', clickTest)
 }
+
 main()
 
-function clickTest(event){
+function clickTest(event) {
     const targetElement = event.composedPath()[0]
-    if(targetElement.classList.contains('building-icon-checkbox')){
+
+    if (targetElement.classList.contains('building-icon-checkbox')) {
         const checked = targetElement.checked
-        //TODO: add building key to data of inputElement
-        console.log(targetElement.id, checked)
+        const buildingElement = event.composedPath()[5]
+        const selected_pms = readBuildingElement(buildingElement)
+        console.log(selected_pms)
+
     } else if (targetElement.classList.contains('pm-radio-button')) {
-        const pm  = targetElement.id
-        const building = targetElement.name
-        //TODO: add building, pmg and pm key to data of inputElement
-        console.log(building, pm)
-
-
-        //const selected_pms = Array.from(targetElement.shadowRoot.querySelectorAll('pmg-choicebox')).map(choiceBox => choiceBox.shadowRoot.querySelector('input[type="radio"]:checked').id)
-        //console.log(selected_pms)
+        const buildingElement = event.composedPath()[8]
+        const selected_pms = readBuildingElement(buildingElement)
+        console.log(selected_pms)
 
     } else {
-        //console.log(targetElement, targetElement.tagName)
+        return;
     }
 
+
+}
+
+function readBuildingElement(buildingElement) {
+    let buildingKey = buildingElement.dataset.buildingKey;
+    let pmList = [];
+    const buildingPmgsElement = buildingElement.shadowRoot.lastElementChild.lastElementChild
+
+    for (const pmg of Array.from(buildingPmgsElement.children)) {
+        for (const pm of Array.from(pmg.shadowRoot.lastElementChild.lastElementChild.children)) {
+            if (pm.type !== 'radio') continue;
+            if (!pm.checked) continue;
+            pmList.push(pm.dataset.pmKey)
+        }
+    }
+
+    return [buildingKey, pmList]
 }
